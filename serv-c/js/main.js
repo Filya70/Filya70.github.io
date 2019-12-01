34914fb8e36e98a5f435e0a2441433516caacadd
Filya70 <49408621+Filya70@users.noreply.github.com>
2019-12-01 17:08:47 +0300

    Finish Desktop commit

--- serv-c/js/main.js
+++ serv-c/js/main.js
@@ -1,8 +1,11 @@
+$(document).ready(function(){
 $('.slider').slick({
     arrows: false,
     dots : true,
     customPaging: (slider, i) => `<a>${'0'+(i + 1) }</a>`,
-    autoplay : true,
+    // autoplay : true,
+    initialSlide: 2,
+    adaptiveHeight: true,
     // autoplaySpeed : 5000,
     // prevArrow: "<img src='img/beforeslide.png' class='prev' alt='1'>",
     // nextArrow: "<img src='img/afterslide.png' class='next' alt='2'>"
@@ -15,4 +18,11 @@ $('.slider').slick({
         adaptiveHeight: true
       }
     }]
+});
+
+$('.question__item__title').on('click',function(){
+  $(this).parents('.question__item').find('.question__item__answ').slideToggle(300);
+  $(this).toggleClass('open');
+});
+
 });

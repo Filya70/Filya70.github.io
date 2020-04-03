bdfe4670619dd33c9b6f5e6685f9aeb1e0cad8d3
Filya70 <49408621+Filya70@users.noreply.github.com>
2020-04-03 12:23:54 +0300

    Add files via upload

---
+++ aviaries/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider__reviews').slick({
+        slidesToShow: 2,
+        arrows: true,
+        prevArrow: "<img src='../img/slider1_l.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='../img/slider1_r.svg' class='next' alt='2'>",
+    });
+});
